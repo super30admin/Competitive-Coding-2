aa47195dc4bade3102ee94bd54b6dcca394d412a
Lavan Kumar <11154769+lavanganji@users.noreply.github.com>
2019-07-14 22:29:57 -0500

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -35,6 +35,31 @@ class Solution {
         // (2) not included 
         else return Math.max( val[n-1] + knapSack(W-wt[n-1], wt, val, n-1), 
                          knapSack(W, wt, val, n-1) 
-                          );      
+                          );  
+        
+        
+        
+      //Dynamic Prog
+      int n = wt.length;
+      int k[][] = new int[n+1][W+1];
+      //construct K array using the tabular form
+      for (int i=0;i<= n ; i++)
+      {
+          for(int w=0;w<=W;w++)
+          {
+              if(n==0 || w ==0 )  k[i][w] = 0;  // initially at weight 0 , all the items will  0 we  will not include any weight to get value.
+              
+              else if(wt[i-1]<= W) 
+                  k[i][w] = Math.max(k[i-1][w] , //previous value 
+                                k[i-1][W-wt[i-1]] +val[i-1]);  // the value of current weight  + Total weight - sum of weights till now.
+              else
+                  k[i][w] = k[i-1][w] ;  //copy the previous value
+            
+          }
+      }
+        
+        return k[n][W] ;
+        
+        
     }
 }
