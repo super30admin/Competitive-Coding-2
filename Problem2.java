24a9439f7d4403e7622651a4bdd8fe6da1b49fa3
Ameya <ameya.dhavalikar@gmail.com>
2019-07-14 22:37:44 -0400

    Problem 2 comments added

--- Problem2.java
+++ Problem2.java
@@ -2,6 +2,23 @@ import java.lang.Math;
 
 //Time complexity: O(n^2) -> n(items)XW(target weight)
 //Space complexity: O(n^2) -> n(items)XW(target weight)
+/*
+val = {60,100,120}
+wt = {10,20,30}
+W = 50
+K[n+1][W+1]
+	  0 1 2 3 ... 10...20...30...  50	
+	0 0 0 0 0     0                 0
+	1 0           60               60
+	2 0           60   100  160    160
+ 	3 0                            220 
+
+K[3][50] = max(val[2]+k[2][20],k[2][50])
+         = max(220,160)
+         =220
+
+*/ 
+
 
 class Knapsack 
 {            
@@ -18,7 +35,8 @@ class Knapsack
 	             if (i==0 || w==0) //row 0(no items to pick) and column 0(Wt :0) will have all entries 0 - base case
 	                  K[i][w] = 0; 
 	             else if (wt[i-1] <= w) //max of current item's value and previously calculated max value
-	                   K[i][w] = Math.max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]); 
+	                 //calculate max value: two options - either include the current item or exclude it 
+	            	 K[i][w] = Math.max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]); 
 	             else
 	                   K[i][w] = K[i-1][w]; 
 	         } 
