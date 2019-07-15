e4b248f5f58ccd0a0af25e73d341f38d11aa3ef6
GAURAV JOSHI <gsj38@msstate.edu>
2019-07-14 21:13:41 -0500

    Updated Solution for Problem1

---
+++ Problem1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution{
+public:
+    vector<int> prob(vector<int>& nums, int target){
+        unordered_map<int, int> mp; 
+        vector<int> returnValue;
+        
+        for(int i = 0; i<nums.size(); i++){
+            if(mp.empty()||mp.find(nums[i])== mp.end())
+                mp.insert(pair<int,int>(target-nums[i],i));
+            else
+            {
+                returnValue.push_back(mp[nums[i]]);
+                returnValue.push_back(i);
+            }
+            
+        }
+        
+        return returnValue;
+    }
+};
+
+int main(){
+    Solution s;
+    vector<int> vec{2,7,11,15};
+    cout<<s.prob(vec,9).at(1)<<endl;
+    return 0;
+}
