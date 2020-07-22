1d7f9343430c95d20fea0f3990f5f8303619632d
Aditya Mathur <adityamathur97@gmail.com>
2020-07-22 03:51:38 -0500

    Added dp solution to Problem 2

--- Problem2.java
+++ Problem2.java
@@ -1,3 +1,5 @@
+// Recursive Solution
+
 // Time Complexity : O(n) , n is length of weight array
 
 // Space Complexity : O(1)
@@ -38,6 +40,47 @@ public class Solution {
 		int[] v = new int[] { 60, 100, 120, 180 };
 		int[] w = new int[] { 10, 20, 30, 20 };
 		Solution obj = new Solution(v, w);
+		System.out.print(obj.findMaxValue(50)); // capacity
+	}
+}
+
+
+// DP Solution
+
+// Time Complexity : O(nT) , n = weight.length+1, T = capacity+1
+
+// Space Complexity : O(nT) 
+
+
+public class Solution4 {
+	int[] v;
+	int[] w;
+
+	Solution4(int[] v, int[] w) {
+		this.v = v;
+		this.w = w;
+	}
+
+	private int findMaxValue(int capacity) {
+		int[][] dp = new int[w.length + 1][capacity + 1];
+
+		for (int i = 1; i <= w.length; i++) {
+			for (int j = 1; j <= capacity; j++) {
+				if (w[i - 1] > j)
+					dp[i][j] = dp[i - 1][j];
+				else {
+					dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w[i - 1]] + v[i - 1]);
+				}
+			}
+		}
+
+		return dp[w.length][capacity];
+	}
+
+	public static void main(String[] args) {
+		int[] v = new int[] { 60, 100, 120, 180 };
+		int[] w = new int[] { 10, 20, 30, 20 };
+		Solution4 obj = new Solution4(v, w);
 		System.out.print(obj.findMaxValue(50)); // capacity
 	}
 }
