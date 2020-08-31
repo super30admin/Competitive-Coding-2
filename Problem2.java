e8809dd5c22d4bb801d46dc814c8e6254190cd8a
Laxmikant Bhaskar Pandhare <47066536+laxmikantbpandhare@users.noreply.github.com>
2020-08-30 22:41:01 -0700

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -22,13 +22,6 @@
 
     */
 
-#### Repatative Sub Problem.
-
-![alt text](https://github.com/kumarrishabh0289/airbnb/blob/laxmikant_new/images/Cancel%20by%20host.png)
-
-#### Matrxi Approach.
-
-![alt text](https://github.com/kumarrishabh0289/airbnb/blob/laxmikant_new/images/Cancel%20by%20host.png)
 
 public class Problem2{
     public static void main(String args[]) {
