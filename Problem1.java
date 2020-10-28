990ff8a21c51206259b64d0e14b0fbc6d3d2a0a1
Pratik Dhumal <pratik.dhumal@netapp.com>
2020-10-27 20:10:32 -0700

    competitive coding-2 submissions

---
+++ Problem1.java
@@ -0,0 +1,23 @@
+//Time O(n)
+//Space O(n)
+class Problem1 {
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+
+        for(int i=0;i<nums.length;i++)
+        {
+            map.put(nums[i],i);
+        }
+
+        for(int i=0;i<nums.length;i++)
+        {
+            int compliment=target-nums[i];
+            if(map.containsKey(compliment)&&i!=map.get(compliment))
+            {
+                return new int[]{i,map.get(compliment)};
+            }
+        }
+        return nums;
+    }
+}
