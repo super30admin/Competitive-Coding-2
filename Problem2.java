c32fb772a2ad9fa300f81b19d2f9401f56ec3749
Rishi Raj <rishiraj824@gmail.com>
2020-12-01 21:23:57 -0500

    Update Problem2.java

---
+++ Problem2.java
@@ -0,0 +1,22 @@
+// Time - O(N)
+// Space - O(N)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        if(nums.length == 0){
+            return new int[]{-1,-1};
+        }
+        int[] res = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<Integer,Integer>();
+        for(int i = 0; i< nums.length ;i++){
+            int compliment = target - nums[i];
+            if(map.containsKey(compliment)){
+                 return new int[] { nums[i], map.get(compliment) };
+            }
+
+            else{
+                map.put(nums[i], i);
+            }
+        }
+        return res;
+    }
+}
