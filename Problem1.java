1f0077b754f09c9f1f1a9e77aba370f086ba48e3
Shivi_Bhatt <in.shivi@gmail.com>
2020-12-03 23:22:00 -0800

    Problem 1 added

---
+++ Problem1.java
@@ -0,0 +1,24 @@
+public class TwoSumProblem1 {
+
+	public int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+		// base case
+		if (nums == null || nums.length == 0)
+			return new int[] { -1, -1 };
+		for (int i = 0; i < nums.length; i++) {
+			map.put(nums[i], i);
+		}
+
+		for (int i = 0; i < nums.length; i++) {
+			int complement = target - nums[i];
+			if (map.containsKey(complement) && map.get(complement) != i) {
+				return new int[] { i, map.get(complement) };
+			}
+		}
+		return new int[] { -1, -1 };
+	}
+
+}
+
+// Time Complexity : O(n)
+// Space Complexity: O(1)
