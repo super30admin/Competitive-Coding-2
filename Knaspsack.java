df62b96c37137c9e67f1ddc1ba41c0258219ceb6
AMAN LADWA <ladwaaman@gmail.com>
2021-05-04 17:02:30 +0530

    Updated KnaspSack

--- Knaspsack.java
+++ Knaspsack.java
@@ -1,3 +1,8 @@
+/*
+Desc:
+Time Complexity: O(n^2) 
+Space complexity: O(n^2)
+*/
 import java.lang.Math;
 public class WeightsnValues{
     public static void main(String []args){
@@ -9,15 +14,15 @@ public class WeightsnValues{
         for(int i=0;i<dp.length;i++){
             for(int j=0;j<dp[0].length;j++){
                 if(i==0 || j==0){
-                    dp[i][j] = 0;
+                    dp[i][j] = 0; //creating buffer for supporting the algorithm ahead
                 }else if(j< wieghts[i-1]){
-                    dp[i][j] = dp[i-1][j];
+                    dp[i][j] = dp[i-1][j]; //Zero case where we do not choose the current wieght, we use the value above it.
                 }else{
-                    dp[i][j] = Math.max(dp[i-1][j],dp[i-1][j-wieghts[i-1]]+values[i-1]);
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i-1][j-wieghts[i-1]]+values[i-1]); //when we use the current wieght we check if the usage of the wieght would lead to maxium value or if the zero case itself is max and pick maximum of them.
                 }
             }
         }
-        System.out.println(dp[dp.length-1][dp[0].length-1]);
+        System.out.println(dp[dp.length-1][dp[0].length-1]); //returning the last element of the matrix would give the maximum value for the capacity given.
     }
     
 }
