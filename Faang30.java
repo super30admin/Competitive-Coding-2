078e3709395e30499b629027adcede7085913ee3
CINTOANTO <63736772+CINTOANTO@users.noreply.github.com>
2021-06-08 00:17:09 +0530

    Update Faang30.java

--- Faang30.java
+++ Faang30.java
@@ -7,24 +7,25 @@ package leetcode;
 public class Faang30 {
     public int maxValue(int[] value,int[] weight, int maxWeight){
         if(value.length==weight.length && value.length==0) return -1;
-        int column = maxWeight/10+1;
-        int sumWeight = 0;
-        int[][] dp = new int[weight.length+1][column];
+
+        int[][] dp = new int[weight.length+1][maxWeight+1];
 
         for(int i=1;i<dp.length;i++){
-            sumWeight += weight[i-1];//sym if weights for each column.
-            for(int j=1;j<column;j++){
-                // value greater than the sums of the weights is zero.
-                if(j*10>sumWeight) break;
-                //value from top row
-                if(j*10 < weight[i-1]){
+            for(int j=1;j<dp[0].length;j++){
+                if(j < weight[i-1]){
                     dp[i][j]=dp[i-1][j];
                 }else{ //maximum between value from top row and sum values between value of current column and value of column that is difference of the weight of  current column.
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-(weight[i-1]/10)]+value[i-1]);
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i-1][j-(weight[i-1])]+value[i-1]);
                 }
             }
         }
-    return dp[dp.length-1][column-1];
+        for(int i=1; i<dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                System.out.print(dp[i][j]);
+            }
+            System.out.println();
+        }
+    return dp[dp.length-1][dp[0].length-1];
     }
 
     public static void main(String[] args) {
@@ -35,3 +36,4 @@ public class Faang30 {
         System.out.println(obj.maxValue(value,weight,maxWeight));
     }
 }
+
