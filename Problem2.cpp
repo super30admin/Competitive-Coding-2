5e101a0d1232e24c9f30cb7c509e566a46c19ab1
Durgesh Patil <patildurgesh1998@gmail.com>
2021-08-03 19:16:45 -0400

    Update Problem2.cpp

--- Problem2.cpp
+++ Problem2.cpp
@@ -23,3 +23,48 @@ int main()
     cout<<helper(v,w,0,0,c)<<endl;
     return 0;
 }
+
+
+//Time Complexity = O(M*N)
+//Space Complexity = O(M*N)
+//Where M is the capacity of knapsack provided and N is the size of the vector weight.
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    //vector<int> v{1,2,6},w{2,3,5};
+    vector<int> v{60,100,120}, w{10,20,30};
+    int i,j;
+    //int c=8;
+    int c = 50;
+    int row = w.size();
+    int col = c;
+    int dp[row+1][col+1];
+    for(i=0;i<=row;i++)
+        dp[i][0] = 0;
+    for(j=0;j<=col;j++)
+        dp[0][j] = 0;
+    
+    for(i=1;i<=row;i++)
+    {
+        for(j=1;j<=col;j++)
+        {
+            if(j<w[i-1])
+                dp[i][j] = dp[i-1][j];
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-w[i-1]]+v[i-1]);       //In dp[i-1][j-w[i-1]]+v[i-1] we are taking i-1 beacause we are not allowed to repeat the weight in the 0/1 knapsack.
+        }
+    }
+    cout<<dp[1][2]<<endl;
+    for(i=1;i<=row;i++)
+    {
+        for(j=1;j<=col;j++)
+            cout<<dp[i][j]<<" ";
+        cout<<endl;
+    }
+    cout<<dp[row][col]<<endl;
+    return 0;
+}
+
+
