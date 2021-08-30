4fbfffdefe32de141ffa02a9a893678b318a31ba
sruthigummadi <45436200+sruthigummadi@users.noreply.github.com>
2021-08-30 14:37:25 -0500

    Completed Competitive Coding 2 - Please Review

---
+++ Problem1.java
@@ -0,0 +1,18 @@
+// Time Complexity : O(N)
+//Space Complexity : O(N)
+
+class Problem1 {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i]))
+            {
+                return new int[] {map.get(target - nums[i]), i};
+            }
+            
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+} 
