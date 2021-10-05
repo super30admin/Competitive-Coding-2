7fa214c2df880aabf482b847a88502036f8e6108
roysouravv30 <roysouravv30@gmail.com>
2021-10-05 19:58:49 -0400

    Comp coding 2 complete

---
+++ knapsack.java
@@ -0,0 +1,39 @@
+// Time Complexity : O(N * W)
+// Space Complexity : O(N * W)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
+// Your code here along with comments explaining your approach
+
+
+static int knapSack(int W, int wt[],
+                        int val[], int n)
+    {
+    //Returns the maximum value that can
+    // be put in a knapsack of capacity W
+        int i, w;
+        int K[][] = new int[n + 1][W + 1];
+ 
+        // Build table K[][] in bottom up manner
+        for (i = 0; i <= n; i++)
+        {
+            for (w = 0; w <= W; w++)
+            {
+                if (i == 0 || w == 0)
+                    K[i][w] = 0;
+                else if (wt[i - 1] <= w)
+                    K[i][w]
+                        = max(val[i - 1]
+                         + K[i - 1][w - wt[i - 1]],
+                         K[i - 1][w]);
+                else
+                    K[i][w] = K[i - 1][w];
+            }
+        }
+ 
+        return K[n][W];
+    }
+    // a function to return the maximum of two integers
+    static int max(int a, int b)
+    {
+          return (a > b) ? a : b;
+    }
