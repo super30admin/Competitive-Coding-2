6667f924c4987922f472a46290354bb7f75b3309
AnjaliJain-17 <anjali.wie@gmail.com>
2021-12-18 12:16:12 -0800

    cp2

---
+++ Problem1.java
@@ -0,0 +1,18 @@
+//space complexity : o(n)
+//time complexity : o(n)
+public class Problem1{
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map= new HashMap<>();
+        for(int i=0; i<nums.length;i++){
+            map.put(nums[i],i);
+        }
+        
+        for(int i=0;i<nums.length;i++){
+            int complement = target- nums[i];
+            if(map.containsKey(complement) && map.get(complement)!=i){
+                return new int[]{i,map.get(complement)};
+            }
+        }
+        return null;
+    }
+}
