eabda779ca286eccf9f02e02a51eca22f627132b
Swaroop Gupta <swaroopgupta.ba@gmail.com>
2022-01-03 16:53:23 -0800

    Completed two sum problem

---
+++ Problem2.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class TwoSum{
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+
+        for(int i = 0 ;i < nums.length; i++){
+            if(numMap.containsKey(nums[i])){
+                return new int[] {i, numMap.get(target - nums[i]) };
+            }else{
+                numMap.put(nums[i], i);
+            }
+        }
+        return new int[]{-1,-1};
+    }
+
+
+    public static void main(String[] args){
+        System.out.println("Number of combinations : "+ twoSum(new int[]{2,7,11,15},  9));
+        System.out.println("Number of combinations : "+ twoSum(new int[]{3,2,4},6));
+    }
+}
