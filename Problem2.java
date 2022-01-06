c508911fac89a9b33c77c18c4d56341b20a6fd92
Swaroop Gupta <swaroopgupta.ba@gmail.com>
2022-01-06 11:01:52 -0800

    Completed competitive coding 2 problems

--- Problem2.java
+++ Problem2.java
@@ -6,7 +6,7 @@ class TwoSum{
         Map<Integer, Integer> numMap = new HashMap<>();
 
         for(int i = 0 ;i < nums.length; i++){
-            if(numMap.containsKey(nums[i])){
+            if(numMap.containsKey(target - nums[i])){
                 return new int[] {i, numMap.get(target - nums[i]) };
             }else{
                 numMap.put(nums[i], i);
