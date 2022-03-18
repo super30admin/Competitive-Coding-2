ab29260c48ccb90d73849b41069c3d737b8d2329
Jeel Patel <JeelPatel@Jeels-MacPro.attlocal.net>
2022-03-18 11:30:31 -0500

    CC- 2 Problem 2

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,26 @@
 // 2 - sum Problem
+
+// Time Complexity : O(N)
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode : YES
+// Any problem you faced while coding this : NO
+
+
+// Your code here along with comments explaining your approach
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i< nums.length; i++){
+            map.put(nums[i], i);
+        }
+        
+        for(int i = 0; i< nums.length; i++){
+            if(map.containsKey(target - nums[i]) && map.get(target - nums[i]) != i){
+                return new int[]{map.get(target - nums[i]), i };
+            }
+        }
+        
+        return new int[]{-1,-1};
+    }
+}
