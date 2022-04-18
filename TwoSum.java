51550048092291c0b8d1a8130ffe8ee78818d187
Rohan <rohantadphale@gmail.com>
2022-04-18 18:41:11 -0400

    #Done

---
+++ TwoSum.java
@@ -0,0 +1,16 @@
+// Time Complexity : O(n) thats length of nums
+// Space Complexity : O(n) for the map
+// Did this code successfully run on Leetcode : yes
+
+class TwoSum{
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[] { map.get(target - nums[i]), i };
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
