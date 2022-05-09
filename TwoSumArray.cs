fa31c2fcc72b5c3f95f2f72210e5629ec617da60
Rincy Soman <rincysoman@gmail.com>
2022-05-09 16:10:16 -0700

    Two sum

---
+++ TwoSumArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+    /// Time Complexity : O(N) 
+    // Space Complexity :O(N) 
+    // Did this code successfully run on Leetcode :Yes
+    // Any problem you faced while coding this :  No
+    public class TwoSumArray
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if (map.ContainsKey(complement))
+                {
+                    return new int[] { map.GetValueOrDefault(complement), i };
+                }
+                if (!map.ContainsKey(nums[i]))
+                {
+                    map.Add(nums[i], i);
+                }
+            }
+            return null;
+
+        }
+    }
+}
