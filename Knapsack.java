7907dc9c6404669a095f1111f23ec0df331c36f1
Aksh-Narayana <99863530+Aksh-Narayana@users.noreply.github.com>
2022-06-07 19:16:10 -0700

    Knapsack recursive

---
+++ Knapsack.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.lang.*;
+class Knapsack{
+    
+    /*static int max(int a, int b)
+    {
+      return (a > b) ? a : b; } */
+    
+static int knapSack(int W, int wt[], int val[], int n)
+{
+
+    if(n == 0 || W == 0)
+        return 0;
+    
+    if (wt[n - 1] > W)
+            return knapSack(W, wt, val, n - 1);
+    
+    int case1 = val[n-1]+knapSack(W-wt[n-1],wt,val,n-1);
+    
+    int case2= knapSack(W,wt,val,n-1);
+    
+    return Math.max(case1,case2);
+    
+    /*return max(val[n - 1]
+					+ knapSack(W - wt[n - 1], wt,
+								val, n - 1),
+					knapSack(W, wt, val, n - 1));*/
+}
+public static void main(String[] args)
+{
+	int val[] = { 60, 100, 120 };
+	int wt[] = { 10, 20, 30 };
+	int W = 50;
+	int n = val.length;
+	System.out.print(knapSack(W, wt, val, n));
+}
+}
