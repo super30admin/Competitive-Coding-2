e1bf0d5d6a95d98b98c4e1b205cbc2c886c4f8a3
tathyapatel <92098849+tathyapatel@users.noreply.github.com>
2022-07-07 18:24:05 -0500

    Create 2Sum.py

---
+++ 2Sum.py
@@ -0,0 +1,17 @@
+
+ """
+ Time Complexity : O(n)
+ Space Complexity : O(1)
+ """
+ class Solution(object):
+     def twoSum(self, nums, target):
+         """
+         :type nums: List[int]
+         :type target: int
+         :rtype: List[int]
+         """
+         d={}
+         for i in range(len(nums)):
+             if target - nums[i] in d :
+                 return [d[target - nums[i]],i]
+             d[nums[i]] = i 
