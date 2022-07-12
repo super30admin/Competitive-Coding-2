c87e256f695b6f1ae731313fe2784519ea35a170
siddbanerjee2012 <sidd.banerjee2012@gmail.com>
2022-07-11 21:59:44 -0500

    Completed Spring Competitive Coding 2 in Java

---
+++ Problem1.java
@@ -0,0 +1,18 @@
+// Time Complexity: O(N)
+// Space Complexity: O(N)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i=0;i<nums.length;i++) {
+            int diff = target - nums[i];
+            if(hm.containsKey(diff)) {
+                return new int[] {hm.get(diff), i};
+            } else {
+                hm.put(nums[i], i);
+            }
+        }
+     return new int[] {};
+    }
+}
