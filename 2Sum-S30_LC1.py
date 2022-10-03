b9425a4298ffd629cd3230562adfaa069f03ee2a
Manan Tarun Shah (Student) <mshah64@sundevils.asu.edu>
2022-10-03 01:47:19 -0700

    Completed

---
+++ 2Sum-S30_LC1.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # Leetcode submission successful.
+        # Time Complexity = O(n)
+        # Space Complexity = O(n)
+
+        hashmap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [hashmap[complement], i]
+            hashmap[nums[i]] = i
