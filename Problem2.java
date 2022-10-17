fc59446bbf09a9b49697fd33d8f7fbceaeb03d72
abhiaher22 <abhiaher22@gmail.com>
2022-10-16 21:28:22 -0700

    Completed

--- Problem2.java
+++ Problem2.java
@@ -1,3 +1,8 @@
+
+// Time Complexity : O(2^n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this : no
 import java.util.Arrays;
 
 public class Problem2 {
