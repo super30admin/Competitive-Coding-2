6b10149b19ea67e74fbd5a325780273cc1dbbaaf
Mohit Nagpal <mohit@Mohits-MBP.attlocal.net>
2022-11-10 01:06:57 -0500

    Completed Competitive-Coding-2

---
+++ Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i]))
+                return new int[] {i, map.get(target - nums[i])};
+            map.put(nums[i],i);
+        }
+        return new int[] {-1,-1};
+    }
+}
