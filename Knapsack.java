ee58575c4f852b19fc823d02f2dbd528ac56e903
aarshilpatel <aarshil.patel@gmail.com>
2022-12-09 00:57:17 -0800

    Completed Competitive-Coding-2

---
+++ Knapsack.java
@@ -0,0 +1,26 @@
+public class Knapsack {
+    public static int knapsack(int[] values, int[] weights, int capacity){
+        if(weights == null || weights.length == 0) return 0;
+        if(values == null || values.length == 0) return 0;
+        int n = weights.length;
+        int [][] dp = new int[n+1][capacity+1];
+        for(int i = 0; i < capacity; i++) dp[0][i] = 0;
+        for(int i = 1; i <= n; i++){
+            for(int j = 0; j <= capacity; j++){
+                if(weights[i-1] <= j){
+                    dp[i][j] = Math.max(values[i-1]+dp[i-1][j-weights[i-1]], dp[i-1][j]);
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][capacity];
+    }
+    public static void main(String[] args) {
+        int[] values = {60,100,120};
+        int[] weights = {10,20,30};
+        int capacity = 50;
+        System.out.println(knapsack(values, weights, capacity));
+    }
+}
---
+++ TwoSum.java
@@ -0,0 +1,22 @@
+// Time Complexity: O(n)
+// Space Complexity = O(1)
+
+/*
+ * here we are going through the array and storing the difference in hashmap
+ * if we find the element in array equal to hashmap key we return the index 
+ * of the element and the value of the hashmap.
+ */
+import java.util.HashMap;
+
+class TwoSum{
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> difference = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(difference.containsKey(nums[i])){
+                return new int[] {difference.get(nums[i]), i};
+            }
+            difference.put(target-nums[i], i);
+        }
+        return new int[] {};
+    }
+}
