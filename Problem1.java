62489cb7d0c6f7a61a382dea7c90294270a16b36
TarunGambhi <118712515+TarunGambhi@users.noreply.github.com>
2022-12-20 15:05:24 +0530

    Update Problem1.java

---
+++ Problem1.java
@@ -0,0 +1,20 @@
+//TC : o(n)
+//SC : o(n)
+//https://leetcode.com/problems/two-sum/
+
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+    int[] result = new int[2];
+		Map<Integer,Integer> map=new HashMap<>();
+		for(int i=0;i<nums.length;i++) {
+			if(map.containsKey(nums[i])) {
+				result[0]=i;
+				result[1]=map.get(nums[i]);
+			}else {
+				map.put(target-nums[i],i);
+			}
+		}
+		return result;
+    }
+}
