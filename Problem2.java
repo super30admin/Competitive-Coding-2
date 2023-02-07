29ee0a3b0b12fdc81ff65701541316895cef50c3
Karan Dhodi <karandhodi@ymail.com>
2023-02-07 11:37:06 -0800

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -47,3 +47,53 @@ public class Knapsack {
 		System.out.println(knapSack(val, wt, 0, W, 0)); //295
 	}
 }
+
+
+// Time Complexity : O(2 ^ n) // where n is number of values
+// Space Complexity : O(n), stack space needed for recursion
+/* A Naive recursive implementation
+of 0-1 Knapsack problem */
+public class Knapsack {
+
+	// Returns the maximum value that
+	// can be put in a knapsack of
+	// capacity W
+	static int knapSack(int[] val, int[] w, int idx, int W, int maxVal)
+	{
+		//System.out.println(idx);
+		//System.out.println(W);
+		if(W < 0)
+		    return -1;
+		//Base case
+		if(W == 0 || idx == val.length)
+		    return maxVal;
+		    
+		
+		    
+		/*if(W < w[idx])
+		    return knapSack(val, w, idx + 1, W);*/
+		    
+		
+		    
+		int case0 = knapSack(val, w, idx + 1, W, maxVal);
+		int case1 = knapSack(val, w, idx + 1, W - w[idx], maxVal + val[idx]);
+		
+		if(case0 == -1)
+		    return case1;
+		if(case1 == -1)
+		    return case0;
+		    
+		return Math.max(case0, case1);
+	}
+
+	// Driver code
+	public static void main(String args[])
+	{
+		int val[] = new int[] { 55, 10, 47, 5, 4, 50, 8, 61, 85, 87 };
+		int wt[] = new int[] { 95, 4, 60, 32, 23, 72, 80, 62, 65, 46 };
+		int W = 269;
+		int n = val.length;
+		System.out.println(knapSack(val, wt, 0, W, 0));
+	}
+}
+
