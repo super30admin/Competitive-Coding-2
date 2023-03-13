69902079550bbc035ab0b6b18ee9a106536264c0
Manasa Nannuru <mnannuru@albanu.edu>
2023-03-13 01:51:11 -0400

    COmpleted sum equal to k in competitive coding 2

---
+++ Problem2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        unordered_map<int,int> umap;
+        vector<int> indices;
+        for (int i=0; i<nums.size();i++){
+            if(umap.find(target-nums[i]) != umap.end()){
+                indices.push_back(i);
+                indices.push_back(umap[target-nums[i]]);
+                return indices;
+            }
+            if(umap.find(nums[i]) == umap.end()){
+                    umap[nums[i]] = i;
+                    cout<< nums[i];
+            }
+        }
+        return indices;
+        
+    }
+};
