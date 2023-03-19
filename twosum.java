cf6d707334d46efd2e421b080de319a36e2de7aa
vagdevij52 <vagdevi.junnuri@sjsu.edu>
2023-03-19 13:48:38 -0700

    cc-2

---
+++ twosum.java
@@ -0,0 +1,16 @@
+public class twosum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int diff = target-nums[i];
+            if(map.containsKey(diff)){
+                return new int[] {i,map.get(diff)};
+            }
+            map.put(nums[i],i);
+        }
+        return null;
+    }
+}
+
+// TC - O(n)
+// SC - O(n)
