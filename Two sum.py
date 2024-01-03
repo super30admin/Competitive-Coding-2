76f203767efae1a3017bd94450b6e647060ec676
Sreedhar Reddy Munagala <sreedharreddy@Sreedhars-Air.lan>
2024-01-02 20:51:19 -0500

    competitive coding 2

---
+++ Two sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_map={}
+
+        for i , num in enumerate(nums):
+            complement= target - num
+
+            if complement in num_map:
+                return [num_map[complement],i]
+            
+            num_map[num]=i
+
+        return [-1,-1]
+
+#Time Complexity: O(N)
+#Space complexity: O(N)
